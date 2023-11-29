2fd9039606cb854cbc7e2d186a7368fbd345c70b
Mohamed Akbarally <49976583+MohamedAkbarally@users.noreply.github.com>
2023-11-29 03:34:28 +0000

    test

---
+++ module_one/hello.py
@@ -0,0 +1,6 @@
+hello = 1
+hello += "str"
+print(
+    "hello my name is mohamed and I am 20 years old and I have 4 kids and they are great and I am ejejje. jrjrj ekjdnewjkndwjkdqdjk "
+)
+print("www")
